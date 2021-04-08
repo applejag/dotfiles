9afa720f0662b52836b21a360ebc70858dfca2c0
Kalle Jillheden <kalle.jillheden@iver.se>
2021-04-08 14:48:26 +0200

    Colored now

--- scripts/timetrap-xfce4-genmon-panel.sh
+++ scripts/timetrap-xfce4-genmon-panel.sh
@@ -1,6 +1,18 @@
 #!/usr/bin/env bash
 
-NOW_OUTPUT="$(timetrap now 2>&1)"
+# https://stackoverflow.com/a/59592881
+{
+    IFS=$'\n' read -r -d '' NOW_INACTIVE;
+    IFS=$'\n' read -r -d '' NOW_ACTIVE;
+} < <((printf '\0%s\0' "$(timetrap now)" 1>&2) 2>&1)
+
+NOW_COLOR='gray'
+NOW_OUTPUT="$NOW_INACTIVE"
+if [ "$NOW_ACTIVE" ]; then
+    NOW_COLOR='lime'
+    NOW_OUTPUT="$NOW_ACTIVE"
+fi
+
 DAY_OUTPUT="$(timetrap display all --format day 2>&1)"
 
 PAT='([0-9]+)%'
@@ -11,4 +23,9 @@ PAT='(([0-9]+:)+[0-9]+)'
 [[ "$DAY_OUTPUT" =~ $PAT ]]
 TIME="${BASH_REMATCH[1]}"
 
-echo "<txt>$NOW_OUTPUT | $TIME </txt><bar>$PERCENTAGE</bar>"
+echo "<txt><span fgcolor='$NOW_COLOR'>$NOW_OUTPUT</span> | $TIME </txt>"
+echo "<bar>$PERCENTAGE</bar>"
+
+# https://git.xfce.org/panel-plugins/xfce4-genmon-plugin/tree/CSS%20Styling.txt
+
+# https://wiki.gnome.org/action/show/Projects/GTK/Inspector?action=show&redirect=Projects%2FGTK%2B%2FInspector
