a67f280706292e03141266b58d30a7775ceb7fe3
Kalle Fagerberg <kalle.fagerberg@iver.se>
2022-01-03 08:25:12 +0100

    Added dinkur gnome executor util

---
+++ scripts/dinkur-gnome-raujonas-executor/go.mod
@@ -0,0 +1,16 @@
+module dinkur-gnome-raujonas-executor
+
+go 1.17
+
+require github.com/dinkur/dinkur v0.0.0-20220102205812-6cc18f0b8e9a
+
+require (
+	github.com/iver-wharf/wharf-core v1.3.0 // indirect
+	github.com/jinzhu/inflection v1.0.0 // indirect
+	github.com/jinzhu/now v1.1.4 // indirect
+	github.com/mattn/go-sqlite3 v1.14.9 // indirect
+	golang.org/x/crypto v0.0.0-20210817164053-32db794688a5 // indirect
+	gopkg.in/yaml.v3 v3.0.0-20210107192922-496545a6307b // indirect
+	gorm.io/driver/sqlite v1.2.6 // indirect
+	gorm.io/gorm v1.22.4 // indirect
+)
---
+++ scripts/dinkur-gnome-raujonas-executor/main.go
@@ -0,0 +1,164 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"html"
+	"io"
+	"os"
+	"os/exec"
+	"path/filepath"
+	"strconv"
+	"strings"
+	"time"
+
+	"github.com/dinkur/dinkur/pkg/dinkur"
+	"github.com/dinkur/dinkur/pkg/dinkurdb"
+	"github.com/dinkur/dinkur/pkg/timeutil"
+)
+
+func main() {
+	home, err := os.UserHomeDir()
+	if err != nil {
+		printErr(err)
+		os.Exit(1)
+	}
+	opt := dinkurdb.Options{
+		MkdirAll:             false,
+		SkipMigrateOnConnect: true,
+		DebugLogging:         false,
+	}
+	db := dinkurdb.NewClient(filepath.Join(home, ".local", "share", "dinkur", "dinkur.db"), opt)
+
+	ctx := context.Background()
+
+	if err := db.Connect(ctx); err != nil {
+		printErr(err)
+		os.Exit(1)
+	}
+
+	tasks, err := db.ListTasks(ctx, dinkur.SearchTask{
+		Shorthand: timeutil.TimeSpanThisDay,
+	})
+	if err != nil {
+		printErr(err)
+		os.Exit(1)
+	}
+
+	var activeTask *dinkur.Task
+	var sumTimes time.Duration
+	for _, task := range tasks {
+		if task.End == nil {
+			activeTask = &task
+		}
+		sumTimes += task.Elapsed()
+	}
+
+	dayLength := time.Hour * 8
+	dayPercentage := int64(100 * sumTimes / dayLength)
+
+	var sb strings.Builder
+	sb.WriteString("<executor.markup.true>")
+	sb.WriteString(" <span foreground='")
+	if activeTask != nil {
+		sb.WriteString("lime")
+	} else {
+		sb.WriteString("gray")
+	}
+	sb.WriteString("'>")
+	if activeTask != nil {
+		sb.WriteString(FormatDuration(activeTask.Elapsed()))
+		if activeTask.Name != "" {
+			sb.WriteString(" (")
+			sb.WriteString(html.EscapeString(activeTask.Name))
+			sb.WriteRune(')')
+		}
+	} else {
+		sb.WriteString("no active task")
+	}
+	sb.WriteString("</span>")
+
+	sb.WriteString(" | <span>")
+	sb.WriteString(FormatDuration(sumTimes))
+	sb.WriteString("</span>")
+	sb.WriteString(" | <span>")
+	sb.WriteString(strconv.FormatInt(dayPercentage, 10))
+	sb.WriteRune('%')
+	sb.WriteString("</span>")
+
+	fmt.Print(sb.String())
+}
+
+func printErr(err error) {
+	var sb strings.Builder
+	sb.WriteString("<executor.markup.true>")
+	sb.WriteString(" <span foreground='red'>err: ")
+	sb.WriteString(err.Error())
+	sb.WriteString("</span>")
+}
+
+type cmdResult struct {
+	stderr string
+	stdout string
+}
+
+func execCmd(name string, args ...string) (cmdResult, error) {
+	cmd := exec.Command(name, args...)
+
+	stderrPipe, err := cmd.StderrPipe()
+	if err != nil {
+		return cmdResult{}, err
+	}
+	stdoutPipe, err := cmd.StdoutPipe()
+	if err != nil {
+		return cmdResult{}, err
+	}
+
+	if err := cmd.Start(); err != nil {
+		return cmdResult{}, err
+	}
+
+	stderr, err := io.ReadAll(stderrPipe)
+	if err != nil {
+		return cmdResult{}, err
+	}
+
+	stdout, err := io.ReadAll(stdoutPipe)
+	if err != nil {
+		return cmdResult{}, err
+	}
+
+	if err := cmd.Wait(); err != nil {
+		return cmdResult{}, err
+	}
+
+	return cmdResult{
+		stdout: string(stdout),
+		stderr: string(stderr),
+	}, nil
+}
+
+func FormatDuration(d time.Duration) string {
+	totalSeconds := int64(d / time.Second)
+	var b []byte
+
+	b = strconv.AppendInt(b, totalSeconds/3600, 10)
+
+	b = append(b, ':')
+
+	minutes := totalSeconds / 60 % 60
+	if minutes < 10 {
+		b = append(b, '0')
+	}
+	b = strconv.AppendInt(b, minutes, 10)
+
+	b = append(b, ':')
+
+	seconds := totalSeconds % 60
+	if seconds < 10 {
+		b = append(b, '0')
+	}
+	b = strconv.AppendInt(b, seconds, 10)
+
+	return string(b)
+}
