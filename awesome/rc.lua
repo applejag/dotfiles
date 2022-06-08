ff9cc91aeb935ecef551aca6d8dcde8bb9ce0ebc
Kalle Fagerberg <kalle.fagerberg@riskident.com>
2022-06-08 09:27:23 +0200

    Changed to spiral as default layout

--- awesome/rc.lua
+++ awesome/rc.lua
@@ -119,6 +119,8 @@ local browser      = "brave-browser"
 awful.util.terminal = terminal
 awful.util.tagnames = { "1", "2", "3", "4", "5" }
 awful.layout.layouts = {
+    awful.layout.suit.spiral,
+    --awful.layout.suit.spiral.dwindle,
     awful.layout.suit.tile,
     awful.layout.suit.tile.left,
     awful.layout.suit.tile.bottom,
@@ -126,8 +128,6 @@ awful.layout.layouts = {
     awful.layout.suit.floating,
     --awful.layout.suit.fair,
     --awful.layout.suit.fair.horizontal,
-    --awful.layout.suit.spiral,
-    --awful.layout.suit.spiral.dwindle,
     --awful.layout.suit.max,
     --awful.layout.suit.max.fullscreen,
     --awful.layout.suit.magnifier,
