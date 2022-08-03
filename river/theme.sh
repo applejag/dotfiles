c82a854a8a596edcecfc8dc0d4e8361ea003d50c
Kalle Fagerberg <kalle.f8@proton.me>
2022-08-03 22:12:49 +0200

    Changed colors

--- river/theme.sh
+++ river/theme.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
 # Set background and border color
-riverctl background-color 0x002b36
-riverctl border-color-focused 0x93a1a1
-riverctl border-color-unfocused 0x586e75
+riverctl background-color 0x002B36
+riverctl border-color-focused 0x2FE833
+riverctl border-color-unfocused 0x586E75
