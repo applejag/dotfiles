0e1b0884405bb89df7c53f741d0f3d937e45b8f7
Kalle Fagerberg <kalle.f8@proton.me>
2022-08-07 11:50:28 +0200

    pactl-set-volume.sh: Added notification on volume change

--- scripts/pactl-set-volume.sh
+++ scripts/pactl-set-volume.sh
@@ -24,3 +24,34 @@ elif [[ "$NEW" -lt 0 ]]; then
 fi
 
 pactl set-sink-volume @DEFAULT_SINK@ "$NEW%"
+
+CACHE_DIR="${HOME}/.cache/pactl-set-volume"
+LAST_NOTI_ID_PATH="${CACHE_DIR}/last_noti_id"
+LAST_NOTI_ID=0
+if [[ -r "$LAST_NOTI_ID_PATH" ]]; then
+    LAST_NOTI_ID="$(cat "$LAST_NOTI_ID_PATH")"
+fi
+
+VOLUME_ICON=''
+URGENCY="normal"
+if [[ "$NEW" -le 15 ]]; then
+    VOLUME_ICON=''
+elif [[ "$NEW" -le 60 ]]; then
+    VOLUME_ICON=''
+elif [[ "$NEW" -gt 100 ]]; then
+    URGENCY="critical"
+fi
+
+NEW_NOTI_ID="$(notify-send \
+    "$VOLUME_ICON Volume $NEW%" \
+    --urgency="$URGENCY" \
+    --app-name="$(basename "$0")" \
+    --expire-time=5000 \
+    --hint=int:value:"$NEW" \
+    --replace-id="$LAST_NOTI_ID" \
+    --print-id)"
+
+mkdir -p "$CACHE_DIR"
+if [[ -w "$CACHE_DIR" ]]; then
+    echo "$NEW_NOTI_ID" > "$LAST_NOTI_ID_PATH"
+fi
