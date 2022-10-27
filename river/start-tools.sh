5fb695c4e3bc94e4b1e45c8c198949f4272173ba
Kalle Fagerberg <kalle.fagerberg@riskident.com>
2022-10-27 13:11:15 +0200

    wlopm: Delay lock slightly after dimming

--- river/start-tools.sh
+++ river/start-tools.sh
@@ -11,8 +11,8 @@ exec_once() {
 exec_once swaybg --image ~/Pictures/Wallpaper/1561845449-201900764-abstraction-nevseoboi.com.ua.jpg
 
 exec_once swayidle -w \
-    timeout 300 'swaylock -f' \
     timeout 300 'wlopm --off "*"' \
+    timeout 310 'swaylock -f' \
     resume 'wlopm --on "*"' \
     before-sleep 'swaylock -f'
 
