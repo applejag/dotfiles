7591fa49ea2abe07277182193382775167a41972
Kalle Fagerberg <kalle.fagerberg@riskident.com>
2023-01-11 11:11:59 +0100

    go: Newer golangci-lint version

--- doom/install-go-deps.sh
+++ doom/install-go-deps.sh
@@ -13,5 +13,4 @@ go install golang.org/x/tools/cmd/goimports@latest # Formatting imports
 
 # golangci-lint is not installed from source as they find it unreliable
 # More info: https://golangci-lint.run/usage/install/#install-from-source
-curl -sSfL https://raw.githubusercontent.com/golangci/golangci-lint/master/install.sh | sh -s -- -b $(go env GOPATH)/bin v1.46.2
-
+curl -sSfL https://raw.githubusercontent.com/golangci/golangci-lint/master/install.sh | sh -s -- -b $(go env GOPATH)/bin v1.50.1
