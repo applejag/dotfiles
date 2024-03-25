db63abcb95b293405872e379a2ec53d6d9fd66f3
Kalle Fagerberg <kalle.fagerberg@riskident.com>
2024-03-25 20:53:03 +0100

    nix: Set locale

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -42,6 +42,21 @@
 
   # Select internationalisation properties.
   i18n.defaultLocale = "en_US.UTF-8";
+  i18n.supportedLocales = [ "all" ];
+  i18n.extraLocaleSettings = {
+    LC_CTYPE = "en_US.UTF-8";
+    LC_NUMERIC = "en_US.UTF-8";
+    LC_TIME = "sv_SE.UTF-8";
+    LC_COLLATE = "en_US.UTF-8";
+    LC_MONETARY = "en_US.UTF-8";
+    LC_MESSAGES = "en_US.UTF-8";
+    LC_PAPER = "sv_SE.UTF-8";
+    LC_NAME = "sv_SE.UTF-8";
+    LC_ADDRESS = "de_DE.UTF-8";
+    LC_TELEPHONE = "sv_SE.UTF-8";
+    LC_MEASUREMENT = "sv_SE.UTF-8";
+    LC_IDENTIFICATION = "en_US.UTF-8";
+  };
   # console = {
   #   font = "Lat2-Terminus16";
   #   keyMap = "us";
