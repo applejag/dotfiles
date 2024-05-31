492a3670d45dcebb85b393772876850de8b93c01
Kalle Fagerberg <kalle.fagerberg@riskident.com>
2024-05-31 09:48:04 +0200

    nvim: yaml injection overrides

--- nvim/after/queries/yaml/injections.scm
+++ nvim/after/queries/yaml/injections.scm
@@ -1,5 +1,48 @@
 ; extends
 
+((comment) @injection.content
+  (#set! injection.language "comment"))
+
+; GitHub Actions workflows
+(block_mapping_pair
+  key: (flow_node) @_script
+  (#any-of? @_script "run")
+  value: (flow_node
+    (plain_scalar
+      (string_scalar) @injection.content)
+    (#set! injection.language "bash")))
+
+(block_mapping_pair
+  key: (flow_node) @_script
+  (#any-of? @_script "run")
+  value: (block_node
+    (block_scalar) @injection.content
+    (#set! injection.language "bash")
+    (#offset! @injection.content 0 1 0 0)))
+
+(block_mapping_pair
+  key: (flow_node) @_script
+  (#any-of? @_script "run")
+  value: (block_node
+    (block_sequence
+      (block_sequence_item
+        (flow_node
+          (plain_scalar
+            (string_scalar) @injection.content))
+        (#set! injection.language "bash")))))
+
+(block_mapping_pair
+  key: (flow_node) @_script
+  (#any-of? @_script "run")
+  value: (block_node
+    (block_sequence
+      (block_sequence_item
+        (block_node
+          (block_scalar) @injection.content
+          (#set! injection.language "bash")
+          (#offset! @injection.content 0 1 0 0))))))
+
+; GitHub Action: actions/github-script
 (block_mapping_pair
   key: (flow_node) @_run
   (#any-of? @_run "script")
