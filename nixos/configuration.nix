f79dba58f8df71652e72172d28e8bda6a8e3d558
Kalle Fagerberg <kalle.fagerberg@riskident.com>
2024-07-26 14:01:23 +0200

    nix: fix hostname

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -29,7 +29,7 @@
 
   boot.tmp.cleanOnBoot = true;
 
-  networking.hostName = "ri-t-0790"; # Define your hostname.
+  networking.hostName = "ri-t-0788"; # Define your hostname.
   # Pick only one of the below networking options.
   #networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
   networking.networkmanager.enable = true;  # Easiest to use and most distros use this by default.
