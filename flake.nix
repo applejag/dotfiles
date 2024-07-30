f1fddc70975fb453b2415c7a38507e602fce2bda
Kalle Fagerberg <kalle.fagerberg@riskident.com>
2024-07-31 01:47:15 +0200

    nix: Disable cosmic defaults

--- flake.nix
+++ flake.nix
@@ -46,7 +46,7 @@
               trusted-public-keys = [ "cosmic.cachix.org-1:Dya9IyXD4xdBehWjrkPv6rtxpmMdRel02smYzA85dPE=" ];
             };
           }
-          nixos-cosmic.nixosModules.default
+          #nixos-cosmic.nixosModules.default
 
           ./nixos/configuration.nix
           ./nixos/hyprland.nix
