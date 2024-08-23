86d92aedd366de530b04a6576c9f2c5a35445804
Kalle Fagerberg <kalle.fagerberg@riskident.com>
2024-08-23 11:52:49 +0200

    nix: Removed flox

--- flake.nix
+++ flake.nix
@@ -81,11 +81,9 @@
             nix.settings = {
               trusted-substituters = [
                 "https://cosmic.cachix.org/"
-                "https://cache.flox.dev"
               ];
               trusted-public-keys = [
                 "cosmic.cachix.org-1:Dya9IyXD4xdBehWjrkPv6rtxpmMdRel02smYzA85dPE="
-                "flox-cache-public-1:7F4OyH7ZCnFhcze3fJdfyXYLQw/aV7GEed86nQ7IsOs="
               ];
             };
           }
