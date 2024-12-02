e5969810c2286e6fc5af7fb8f959f63a46d71e42
Kalle Fagerberg <kalle.fagerberg@riskident.com>
2024-12-02 08:47:28 +0100

    nix: enable opam/OCaml

--- nixos/home/home.nix
+++ nixos/home/home.nix
@@ -226,6 +226,11 @@
     enableZshIntegration = true;
   };
 
+  programs.opam = {
+    enable = true;
+    enableZshIntegration = true;
+  };
+
   services.ssh-agent.enable = true;
   programs.ssh = {
     enable = true;
