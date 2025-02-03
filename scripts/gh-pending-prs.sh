c99a152b102dfee404217375c01488833e5571cd
Kalle Fagerberg <kalle.fagerberg@riskident.com>
2025-02-03 11:33:02 +0100

    gh script: change to nerdfont icons

--- scripts/gh-pending-prs.sh
+++ scripts/gh-pending-prs.sh
@@ -5,7 +5,7 @@ GH_USER="$(yq ".[\"$GH_HOST\"].user" ~/.config/gh/hosts.yml)"
 echo "Using user: $GH_USER" >&2
 
 if ! PRS_TO_REVIEW="$(gh search prs --review-requested "$GH_USER" --state open --review required --archived=false --draft=false --json id --template '{{len .}}')"; then
-	echo -n "<b>[GitHub]</b> "
+	echo -n "<b></b>  "
 	echo -n "<span foreground='red'>failed to load</span>"
 	exit 0
 fi
@@ -13,28 +13,28 @@ fi
 PRS_TO_MERGE="$(gh search prs --author "$GH_USER" --state open --review approved --archived=false --draft=false --json id --template '{{len .}}')"
 PRS_TO_CHANGE="$(gh search prs --author "$GH_USER" --state open --review changes_requested --archived=false --draft=false --json id --template '{{len .}}')"
 
-echo -n "<b>[GitHub]</b> "
+echo -n "<b></b>  "
 
 if [[ "$PRS_TO_REVIEW" == 0 ]]; then
-	echo -n "<span foreground='gray'>review: $PRS_TO_REVIEW</span>"
+	echo -n "<span foreground='gray'> $PRS_TO_REVIEW</span>"
 else
-	echo -n "<span foreground='orange'>review: $PRS_TO_REVIEW</span>"
+	echo -n "<span foreground='orange'> $PRS_TO_REVIEW</span>"
 fi
 
-echo -n " | "
+echo -n " "
 
 if [[ "$PRS_TO_MERGE" == 0 ]]; then
-	echo -n "<span foreground='gray'>merge: $PRS_TO_MERGE</span>"
+	echo -n "<span foreground='gray'> $PRS_TO_MERGE</span>"
 else
-	echo -n "<span foreground='green'>merge: $PRS_TO_MERGE</span>"
+	echo -n "<span foreground='green'> $PRS_TO_MERGE</span>"
 fi
 
-echo -n " | "
+echo -n " "
 
 if [[ "$PRS_TO_CHANGE" == 0 ]]; then
-	echo -n "<span foreground='gray'>change: $PRS_TO_CHANGE</span>"
+	echo -n "<span foreground='gray'> $PRS_TO_CHANGE</span>"
 else
-	echo -n "<span foreground='red'>change: $PRS_TO_CHANGE</span>"
+	echo -n "<span foreground='red'> $PRS_TO_CHANGE</span>"
 fi
 
 echo
