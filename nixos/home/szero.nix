bd0999f0f047530b9010b34be57dd927f2d3351e
Kalle Fagerberg <kalle.fagerberg@riskident.com>
2025-03-17 20:05:13 +0100

    nix: flake update

--- nixos/home/szero.nix
+++ nixos/home/szero.nix
@@ -4,7 +4,7 @@ let
     pname = "szero";
     version = jadolg-szero-src.rev;
     src = jadolg-szero-src;
-    vendorHash = "sha256-TvXJS+X75hvjmRzkA2h+niacoK0IkTQQhaAgmjeg35g=";
+    vendorHash = "sha256-abhLrb7nvLi0OW0VhB95XDjQ9rAc5xynNps34W++9jg=";
   };
 in
 {
