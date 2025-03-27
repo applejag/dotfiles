333bf42627bffb1be7ecad1b49aaa35a89caa49c
Kalle Fagerberg <kalle.fagerberg@riskident.com>
2025-03-27 08:55:14 +0100

    nix: Fix auto-cpufreq

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -417,6 +417,21 @@
 
   services.auto-cpufreq.enable = lib.mkDefault true;
 
+  nixpkgs.overlays = [
+      (final: prev: {
+        # FIX: https://github.com/NixOS/nixpkgs/issues/392278
+        auto-cpufreq = prev.auto-cpufreq.overrideAttrs (oldAttrs: {
+          postPatch =
+            oldAttrs.postPatch
+            + ''
+
+              substituteInPlace pyproject.toml \
+              --replace-fail 'psutil = "^6.0.0"' 'psutil = ">=6.0.0,<8.0.0"'
+            '';
+        });
+      })
+  ];
+
   services.fwupd.enable = true;
 
   services.clamav = {
