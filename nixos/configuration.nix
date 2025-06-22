f1940554e701587cc02eec471c72aa57d7d1a7a5
Kalle Fagerberg <kalle.fagerberg@riskident.com>
2025-06-22 20:21:25 +0200

    nix: more ports

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -438,6 +438,7 @@
   networking.firewall.enable = true;
   networking.firewall.allowedTCPPortRanges = [
     { from = 8000; to = 8100; }
+    { from = 10000; to = 20000; }
   ];
 
   # Copy the NixOS configuration file and link it from the resulting system
